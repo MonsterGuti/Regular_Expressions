2c0a7cc04063776ed1db87262ed8af0aeb7fbcf1
MonsterGuti <gogulanovmartin@gmail.com>
2025-03-16 22:32:26 +0200

    Create capture_the_numbers.py

---
+++ regular_expressions_Exercise/capture_the_numbers.py
@@ -0,0 +1,10 @@
+import re
+
+line = input()
+
+while line:
+    regex = r"\d+"
+    matches = re.findall(regex, line)
+    if matches:
+        print(" ".join(matches), end=" ")
+    line = input()
